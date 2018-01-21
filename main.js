0c8129b6db4e9c0c6c4eb33fcc7bfafcdd216c69
Shixun <shixun.liu1023@gmail.com>
2018-01-22 08:50:00 +1100

    fill rect in canvas

--- main.js
+++ main.js
@@ -1,34 +1,38 @@
 window.onload = function() {
-    var div = document.getElementById('canvas');
-    var painting = false;
-    div.onmousedown = function(a) {
-        painting = true;
-        // var x = a.clientX;
-        // var y = a.clientY;
-        // var divA = document.createElement('div');
-        // divA.style = "width:6px; height:6px;" + 
-        // "background:black; border-radius:3px;" +
-        // "position:absolute; left: " + (x-3) +"px;" + 
-        // "top: " + (y-3) + "px;";
-        // div.appendChild(divA);
-    }; 
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext('2d');
+    context.fillStyle='red';
+    context.fillRect(0,0,100,100);
+    // var div = document.getElementById('canvas');
+    // var painting = false;
+    // div.onmousedown = function(a) {
+    //     painting = true;
+    //     // var x = a.clientX;
+    //     // var y = a.clientY;
+    //     // var divA = document.createElement('div');
+    //     // divA.style = "width:6px; height:6px;" + 
+    //     // "background:black; border-radius:3px;" +
+    //     // "position:absolute; left: " + (x-3) +"px;" + 
+    //     // "top: " + (y-3) + "px;";
+    //     // div.appendChild(divA);
+    // }; 
 
-    div.onmousemove = function(a) {
-        if(painting) {
-            var x = a.clientX;
-            var y = a.clientY;
-            var divA = document.createElement('div');
-            divA.style = "width:6px; height:6px;" + 
-            "background:black; border-radius:3px;" +
-            "position:absolute; left: " + (x-3) +"px;" + 
-            "top: " + (y-3) + "px;";
-            div.appendChild(divA);
-        } else {
+    // div.onmousemove = function(a) {
+    //     if(painting) {
+    //         var x = a.clientX;
+    //         var y = a.clientY;
+    //         var divA = document.createElement('div');
+    //         divA.style = "width:6px; height:6px;" + 
+    //         "background:black; border-radius:3px;" +
+    //         "position:absolute; left: " + (x-3) +"px;" + 
+    //         "top: " + (y-3) + "px;";
+    //         div.appendChild(divA);
+    //     } else {
 
-        } 
-    };
+    //     } 
+    // };
     
-    div.onmouseup = function(z) {
-        painting = false;
-    };
+    // div.onmouseup = function(z) {
+    //     painting = false;
+    // };
 };
